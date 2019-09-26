13c90978af73275293c1ccc6a54db42863a5bfae
veena.Ravi <veena97ravi@gmail.com>
2019-09-26 20:06:24 +0530

    Added my project

---
+++ AllotBedViewModelTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AllotBedViewModelTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AllotBedViewModelTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("923c88b9-e274-4d4d-9547-97d13780702d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AllotBedViewModelTest/UnitTest1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using AllotBedModuleLib;
+using EnableVitalsClass;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VitalSigns;
+
+namespace AllotBedViewModelTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+       
+        public void Given_PatientId_And_Vitals_To_api_should_return_Success()
+        {
+            AllotBedViewModel allotBed = new AllotBedViewModel();
+            SetVitals set = new SetVitals();
+            set.PatientId = "veena";
+            set.VitalsSigns = new List<VitalSign>();
+            set.VitalsSigns.Add(new VitalSign { IsEnabled = true, Type = 0 });
+            bool expected = true;
+            HttpClient client = new HttpClient();
+            var response = client.PostAsJsonAsync
+                ("http://localhost:58905/api/BedConfiguration/ConfigureBed", 234).Result;
+            bool actual = allotBed.AllotBed(set.PatientId, set);
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+
+        public void Test_EnableVitals1()
+        {
+            AllotBedViewModel bed = new AllotBedViewModel();
+            bed.PatientId = "boo";
+            VitalSign vital = new VitalSign
+            {
+                IsEnabled = true,
+                Type = 0
+            };
+            bed.EnableVitals1();
+            Assert.AreEqual("boo", bed.PatientId);
+        }
+    }
+}
